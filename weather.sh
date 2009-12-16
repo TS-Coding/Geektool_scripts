395a61b378135fe54dd3d4afcf56cea7d62c9f79
TS-Coding <Tobiasschug@gmail.com>
2009-12-16 16:38:36 +0100

    Yahoo url fixes for picture tagging

--- weather.sh
+++ weather.sh
@@ -9,15 +9,17 @@
 ##		Funktion gegeben
 ##	Version 0.2
 ##		Unnötige Operationen verhindert
+##	Version 0.3
+##		Url Fix
 #####################################
 
 
 #path where u save the picture
-user="/Users/Tobias/Documents/GeekTool/weather.jpg"
+user="$HOME/Documents/GeektoolScripts/weather.jpg"
 
 #default Yahoo site where u can get the right url (I coded it for °C)
 #http://weather.yahoo.com
-picture=$(curl --silent http://weather.yahoo.com/Stuttgart-Germany/GMXX0128/forecast.html?unit=c | grep background:url | sed -e "s/.*url('//; s/').*//")
+picture=$(curl --silent http://ca.weather.yahoo.com/forecast/GMXX1144.html | grep '<div id="weather_icon"' | sed -e "s?.*url(??; s?).*??")
 
 # download only if internet connection is available
 if [ "$picture" != "" ]
@@ -28,12 +30,12 @@ then
 fi
 
 #get temp
-temp=$(curl --silent http://weather.yahoo.com/Stuttgart-Germany/GMXX0128/forecast.html?unit=c | grep 'Feels Like:' | sed -e 's/.*<dd>//;s/&.*//')
+temp=$(curl --silent http://ca.weather.yahoo.com/forecast/GMXX1144.html | grep -A 1 'Feels Like:' | sed -e 's/.*<dd>//;s/&.*//' | sed -e 's?.*</dt>??')
 
 # write 100 C if internet connection is not available 
 if [ "$temp" == "" ]
 then
 	echo "100 C"
 else
-	echo $temp C	
+	echo Temperatur: $temp C	
 fi
